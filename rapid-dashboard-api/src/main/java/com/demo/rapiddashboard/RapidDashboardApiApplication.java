26c8a223eac5471116a6b40b4ffffdab57d46a50
Sumit Gupta <sumitgpt@users.noreply.github.com>
2021-03-02 00:28:07 +0530

    Setup new backend and frontend for rapid dashboard

---
+++ rapid-dashboard-api/src/main/java/com/demo/rapiddashboard/RapidDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.rapiddashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidDashboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RapidDashboardApiApplication.class, args);
+    }
+
+}
---
+++ rapid-dashboard-api/src/test/java/com/demo/rapiddashboard/RapidDashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.rapiddashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidDashboardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
